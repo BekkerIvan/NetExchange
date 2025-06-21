70de74b566d2a704b59bc783830ff3e044812805
Ivan-Bekker <ivan.bekker@stratusolve.com>
2025-06-21 14:12:18 +0200

    Server basic functions done

--- server/assets/php/currencies/currency.base.php
+++ server/assets/php/currencies/currency.base.php
@@ -1,3 +1,10 @@
 <?php
 abstract class CurrencyBase {
+    public static function convert(float $ValueFlt, float $ExchangeRateFlt, int $RoundInt = 2): float {
+        return round($ValueFlt * $ExchangeRateFlt, $RoundInt);
+    }
+
+    public static function format(float $ValueFlt, int $DecimalsInt = 2, string $DecimalSeparatorStr = ".", $ThousandSeparatorStr = " "): string {
+        return number_format($ValueFlt, $DecimalsInt, $DecimalSeparatorStr, $ThousandSeparatorStr);
+    }
 }
